1629748edb93400f08335882f686fbe65e90371f
sczahra <sczahra@gmail.com>
2026-06-01 16:30:50 -0400

    Bump version to 0.3.3 and add new-feature tab registry

--- mailscan2026/__init__.py
+++ mailscan2026/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.3.2"
+__version__ = "0.3.3"
+
+# Tabs listed here get a one-version red-dot marker in the GUI.
+# Rule: when a previously disabled tab gets a real feature, add its tab name here.
+# On the following version, remove it.
+NEW_FEATURE_TABS: tuple[str, ...] = ()
