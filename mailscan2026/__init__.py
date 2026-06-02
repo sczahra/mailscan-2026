302839e1fd37c5b787519b563220548578d46a18
sczahra <sczahra@gmail.com>
2026-06-02 13:06:15 -0400

    Bump version to 0.12.0 for review mode

--- mailscan2026/__init__.py
+++ mailscan2026/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.11.0"
+__version__ = "0.12.0"
 
 # Tabs listed here get a one-version red-dot marker in the GUI.
 # Rule: when a previously disabled tab gets a one-version red-dot marker.
 # On the following version, remove it.
-NEW_FEATURE_TABS: tuple[str, ...] = ("Documents", "Settings", "Wiki")
+NEW_FEATURE_TABS: tuple[str, ...] = ("Documents", "Dashboard", "Settings", "Wiki")
