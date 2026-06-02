1d4340b785da483d43cff4c2a38f9c925c60f10b
sczahra <sczahra@gmail.com>
2026-06-02 10:00:14 -0400

    Bump version to 0.10.0 for visual triage

--- mailscan2026/__init__.py
+++ mailscan2026/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.3"
+__version__ = "0.10.0"
 
 # Tabs listed here get a one-version red-dot marker in the GUI.
 # Rule: when a previously disabled tab gets a one-version red-dot marker.
